ccf385418619fe4a46adcb91933eef52abb0d595
Undead9000 <undead10000@gmail.com>
2020-01-30 18:30:54 +0200

    Replased HTML with template strings

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
 const API_KEY = 'O3iJIz4KNHqyrwfM88y7Abn9WA2607z0';
+const NUMBER_OF_IMAGES = 8;
 
 let searchCategory = 'cats';
-let url = 'http://api.giphy.com/v1/gifs/search?q=' + searchCategory + '&api_key=' + API_KEY + '&limit=8';
-let listItems = $('.list li');
+let url = 'http://api.giphy.com/v1/gifs/search?q=' + searchCategory + '&api_key=' + API_KEY + '&limit=' + NUMBER_OF_IMAGES;
+let list = $('.list');
 
 get(url).then(function(response) {
     let responseObject = JSON.parse(response);
-    listItems.each(function(index){
-        render($(this), responseObject.data[index]);
-    })
+    for (let key in responseObject.data){
+        list.append(LiTemplate(responseObject.data[key]))
+    }
 }, function(error) {
     console.error("Failed!", error);
 });
 
-$('.list__more').on('click',function(){
+list.on('click', '.list__more', function(){
     $(this).siblings('.list__size').fadeToggle('fast');
     $(this).siblings('.list__link').fadeToggle('fast');
 });
 
 $('.query__random').on('click',function(e){
     e.preventDefault();
-    listItems.each(function(){
-        let thisLi = $(this);
+    list.children().remove();
+    for(let i = 0; i < NUMBER_OF_IMAGES; i++) {
         url = 'http://api.giphy.com/v1/gifs/random?tag=' + searchCategory + '&api_key='+ API_KEY;
         get(url).then(function(response) {
             responseObject = JSON.parse(response);
-            render(thisLi, responseObject.data)
+            list.append(LiTemplate(responseObject.data))
         }, function(error) {
             console.error("Failed!", error);
         });
-    })
+    }
 });
 
 $('.query__search').on('change paste keyup',function(){
     if($(this).val().length > 2) {
+        list.children().remove();
         url = 'http://api.giphy.com/v1/gifs/search?q=' + $(this).val() + '&api_key=' + API_KEY + '&limit=8';
         get(url).then(function(response) {
             responseObject = JSON.parse(response);
-            if(responseObject.data.length) {
-                listItems.each(function(index){
-                    render($(this), responseObject.data[index]);
-                })
+            for (let key in responseObject.data){
+                list.append(LiTemplate(responseObject.data[key]))
             }
         }, function(error) {
             console.error("Failed!", error);
@@ -48,11 +48,21 @@ $('.query__search').on('change paste keyup',function(){
     }
 });
 
-function render(self, item) {
-    self.find('img').attr('src', item.images.original.url);
-    self.find('.list__name').text(item.title);
-    self.find('.list__size span').text(item.images.original.size);
-    self.find('.list__link').attr('href', item.images.original.url);
+function LiTemplate(current) {
+    return `<li>
+                <img src="${current.images.original.url}" alt="img">
+                <div class="list__info">
+                    <span class="list__name">${current.title}</span>
+                    <div class="list__more">i</div>
+                    <div class="list__size">
+                        Size:
+                        <span>${current.images.original.size}</span>
+                        bytes
+                    </div>
+                    <a href="${current.images.original.url}" class="list__link" target="_blank">link</a>
+                </div>
+            </li>
+    `;
 }
 
 function get(url) {
